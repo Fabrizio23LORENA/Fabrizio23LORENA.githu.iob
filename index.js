c1c05ad7707c99c22e1e3838810ad7e7f5896189
Fabrizio23LORENA <159809536+Fabrizio23LORENA@users.noreply.github.com>
2024-02-12 11:11:28 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const nobtn = document.querySelector('#nobtn');
+
+nobtn.addEventListener('mouseover',function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    nobtn.style.setProperty('top',randomY+'%');
+    nobtn.style.setProperty('left',randomX+'%');
+    nobtn.style.setProperty('transform','traslate(-${randomX}%,-${randomY}%)');
+})
